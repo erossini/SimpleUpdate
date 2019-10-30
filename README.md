a62515b77412bab4a7c755c2cb28bd64cc493b83
Enrico <erossini@vubiquity.com>
2019-10-30 12:31:17 +0000

    First update

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
 # SimpleUpdate
 Simple update with drag &amp; drop in ASP.NET Core webapp to Azure Storage
+
+To summarize what the code does, here’s a step-by-step explanation:
+
+* Upload a file submitted in a web browser via HTTP POST.
+* Convert the file to a byte array.
+* Save the file data to Azure Blob Storage.
+
+Let’s take a deeper look at the code.
+
+1. The Index view under the Home controller displays one input field for selecting files for upload, and a submit button to complete the upload process.
+
+Here’s a snippet:
+
+```
+<form enctype="multipart/form-data" method="post">
+...
+   <input multiple="multiple" name="files" type="file" />
+...
+   <input type="submit" value="Upload" />
+...
+</form>
+```
+
+2. Next, the Post method of the Home controller handles the uploaded file. It’s set up to loop through multiple uploaded files, and upload each file separately. Updated from the original blog post, there are now two options.
+
+* Option A: convert to byte array before upload
+* Option B: read directly from stream for blob upload
+
+The UploadToBlob() method still takes in the filename. But now, it can take either take in the byte array or the stream of data from the uploaded file. If one of them is set to null, the other will be used. If both are null, the method will return false.
+
+```
+// NOTE: uncomment either OPTION A or OPTION B to use one approach over another
+
+// OPTION A: convert to byte array before upload
+//using (var ms = new MemoryStream())
+//{
+// formFile.CopyTo(ms);
+// var fileBytes = ms.ToArray();
+// uploadSuccess = await UploadToBlob(formFile.FileName, fileBytes, null);
+
+//}
+
+// OPTION B: read directly from stream for blob upload 
+using (var stream = formFile.OpenReadStream())
+{
+ uploadSuccess = await UploadToBlob(formFile.FileName, null, stream);
+}
+```
+
+You could change the code to upload them all to the same container, but that is up to you.
+
+3. Finally, the UploadToBlob() method in the Home controller uploads the image data to a storage container in three stages: create a CloudBlobClient object to represent the storage account, create a container within that storage account and sets its permissions, upload the image data.
+
+```
+CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+
+...
+cloudBlobContainer = cloudBlobClient.GetContainerReference("uploadblob" + Guid.NewGuid().ToString());
+await cloudBlobContainer.CreateAsync();
+
+... 
+BlobContainerPermissions permissions = new BlobContainerPermissions
+{
+   PublicAccess = BlobContainerPublicAccessType.Blob
+};
+await cloudBlobContainer.SetPermissionsAsync(permissions);
+
+...
+CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(filename);
+
+if (imageBuffer != null)
+{
+   // OPTION A: use imageBuffer (converted from memory stream)
+   await cloudBlockBlob.UploadFromByteArrayAsync(imageBuffer, 0, imageBuffer.Length);
+}
+else if (stream != null)
+{
+   // OPTION B: pass in memory stream directly
+   await cloudBlockBlob.UploadFromStreamAsync(stream);
+} else
+{
+   return false;
+}
+
+return true;
+```
+
+For obvious security reasons, the connection string for the storage connection is kept in a config file that is not included in the Github repo. Instead there is a placeholder config file that can be renamed and updated to refer to any storage account that you own.
+From the Readme file, the instructions are:
+
+* Rename placeholder config file “appsettings.Development.txt” to “appsettings.Development.json”
+* Replace placeholder string “<REPLACE_CONN_STRING>” with your Azure Storage Account connection string.
+
+## Drag and drop
+
+Another part of the code is for the drag and drop.
---
+++ SimpleUpload/Controllers/HomeController.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using SimpleUpload.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SimpleUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public HomeController(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("UploadFiles")]
+        //OPTION A: Disables Asp.Net Core's default upload size limit
+        [DisableRequestSizeLimit]
+        //OPTION B: Uncomment to set a specified upload file limit
+        //[RequestSizeLimit(40000000)] 
+
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            var uploadSuccess = false;
+            string uploadedUri = null;
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length <= 0)
+                {
+                    continue;
+                }
+
+                // NOTE: uncomment either OPTION A or OPTION B to use one approach over another
+
+                // OPTION A: convert to byte array before upload
+                //using (var ms = new MemoryStream())
+                //{
+                //    formFile.CopyTo(ms);
+                //    var fileBytes = ms.ToArray();
+                //    uploadSuccess = await UploadToBlob(formFile.FileName, fileBytes, null);
+
+                //}
+
+                // OPTION B: read directly from stream for blob upload      
+                using (var stream = formFile.OpenReadStream())
+                {
+                    (uploadSuccess, uploadedUri) = await UploadToBlob(Path.GetFileName(formFile.FileName), null, stream);
+                    TempData["uploadedUri"] = uploadedUri;
+                }
+                
+            }
+
+            if (uploadSuccess)
+                return View("UploadSuccess");
+            else
+                return View("UploadError");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadMyFiles()
+        {
+            long size = 0;
+            var files = Request.Form.Files;
+
+            var uploadSuccess = false;
+            string uploadedUri = null;
+
+            foreach (var file in files)
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    (uploadSuccess, uploadedUri) = await UploadToBlob(Path.GetFileName(file.FileName), null, stream);
+                    TempData["uploadedUri"] = uploadedUri;
+                }
+            }
+
+            string message = $"{files.Count} file(s)/{ size} bytes uploaded successfully!";
+            return Json(message);
+        }
+
+        private async Task<(bool, string)> UploadToBlob(string filename, byte[] imageBuffer = null, Stream stream = null)
+        {
+            CloudStorageAccount storageAccount = null;
+            CloudBlobContainer cloudBlobContainer = null;
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=explorerteststorage;AccountKey=YqiP5e0rRG5oEKzgRTF3WQ2iclRVaa1gmXVzBF9CWLkED6ijzE3N8iES+qPiRBzg/CjYKrlvimbu+hAIxxKXtw==;EndpointSuffix=core.windows.net"; // _configuration["storageconnectionstring"];
+
+            // Check whether the connection string can be parsed.
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                try
+                {
+                    // Create the CloudBlobClient that represents the Blob storage endpoint for the storage account.
+                    CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+
+                    // Create a container called 'uploadblob' and append a GUID value to it to make the name unique. 
+                    cloudBlobContainer = cloudBlobClient.GetContainerReference("uploads");
+                    await cloudBlobContainer.CreateIfNotExistsAsync();
+
+                    // Set the permissions so the blobs are public. 
+                    BlobContainerPermissions permissions = new BlobContainerPermissions
+                    {
+                        PublicAccess = BlobContainerPublicAccessType.Blob
+                    };
+                    await cloudBlobContainer.SetPermissionsAsync(permissions);
+
+                    // Get a reference to the blob address, then upload the file to the blob.
+                    CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(filename);
+
+                    if (imageBuffer != null)
+                    {
+                        // OPTION A: use imageBuffer (converted from memory stream)
+                        await cloudBlockBlob.UploadFromByteArrayAsync(imageBuffer, 0, imageBuffer.Length);
+                    }
+                    else if (stream != null)
+                    {
+                        // OPTION B: pass in memory stream directly
+                        await cloudBlockBlob.UploadFromStreamAsync(stream);
+                    } else
+                    {
+                        return (false, null);
+                    }
+
+                    return (true, cloudBlockBlob.SnapshotQualifiedStorageUri.PrimaryUri.ToString());
+                }
+                catch (StorageException ex)
+                {
+                    return (false, null);
+                }
+                finally
+                {
+                    // OPTIONAL: Clean up resources, e.g. blob container
+                    //if (cloudBlobContainer != null)
+                    //{
+                    //    await cloudBlobContainer.DeleteIfExistsAsync();
+                    //}
+                }
+            }
+            else
+            {
+                return (false, null);
+            }
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
